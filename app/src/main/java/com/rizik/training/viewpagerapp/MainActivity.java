4091bac57b3652478f40ba6f0f5346a2147f306a
rizikra <rizikardi01@gmail.com>
2020-01-31 17:28:39 +0700

    Penggunaan Switch-Case dan Generate-add

--- app/src/main/java/com/rizik/training/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/rizik/training/viewpagerapp/MainActivity.java
@@ -1,24 +1,53 @@
 package com.rizik.training.viewpagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
     private MyAdapter myAdapter;
+    private TabLayout tabLayout;
+
+    ArrayList<String> menuTitleData = new ArrayList<>();
+    ArrayList<String> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        generateTitle();
+        generateData();
+
         //Panggil viewPager yang ada di activity_main ke Main Activity
         viewPager = findViewById(R.id.viewPager);
-        myAdapter = new MyAdapter(getSupportFragmentManager());
-
+        //myAdapter = new MyAdapter(getSupportFragmentManager());
+        myAdapter = MyAdapter.newInstance(getSupportFragmentManager(), menuTitleData, data);
         viewPager.setAdapter(myAdapter);
+
+        //Tambahkan TabLayout
+        tabLayout = findViewById(R.id.tab_menu);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
+    //Membuat dummy title data
+    private void generateTitle(){
+        menuTitleData.add("T dot");
+        menuTitleData.add("Techpolitan");
+        menuTitleData.add("ZONK");
+    }
+    private void generateData(){
+        data.add("Data 1");
+        data.add("Data 2");
+        data.add("Data 3");
+    }
+
 }
--- app/src/main/java/com/rizik/training/viewpagerapp/MyAdapter.java
+++ app/src/main/java/com/rizik/training/viewpagerapp/MyAdapter.java
@@ -1,12 +1,28 @@
 package com.rizik.training.viewpagerapp;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
 // SUBCLASS DARI ACTIVITY MAIN
 public class MyAdapter extends FragmentStatePagerAdapter {
+    private static ArrayList<String> myTitles;
+    private static ArrayList<String> myData;
+
+    private static int numOfItem;
+
+    public static MyAdapter newInstance (FragmentManager fragmentManager, ArrayList<String> titles, ArrayList<String> data){
+        MyAdapter myAdapter = new MyAdapter(fragmentManager);
+        myTitles = titles;
+        myData = data;
+        numOfItem = data.size();
+        return myAdapter;
+    }
+    //Constractor
     public MyAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
     }
@@ -20,22 +36,30 @@ public class MyAdapter extends FragmentStatePagerAdapter {
 
         Fragment item = null;
 
-        switch (position){
+        switch (position) {
             case 0:
                 item = fragmentSatu;
                 break;
             case 1:
                 item = fragmentDua;
                 break;
-                default:
-                    item = fragmentSatu;
-                    break;
+            default:
+                item = fragmentSatu;
+                break;
+
         }
         return item;
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return numOfItem;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        String title = myTitles.get(position);
+        return title;
     }
 }
